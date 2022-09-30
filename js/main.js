dca39ed0901166a0fabd48688494f49c9ee41346
Snezhanna Roshchupkina <Snezhanna.Roshchupkina@lanit-tercom.com>
2022-09-30 19:36:47 +0300

    Started adaptive layout: added burger menu

--- js/main.js
+++ js/main.js
@@ -10,3 +10,18 @@
         }
     }
 }());
+
+
+// Burger menu handler
+(function () {
+    const burgerItem = document.querySelector('.burger');
+    const menu = document.querySelector('.header__nav');
+    const menuCloseItem = document.querySelector('.header__nav-close');
+    burgerItem.addEventListener(
+        'click', () => {menu.classList.add('header__nav_active');
+    });
+    menuCloseItem.addEventListener(
+        'click', () => {menu.classList.remove('header__nav_active');
+    });
+
+}());
